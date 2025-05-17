918bc91a7c2ea98ef3f1c3d3184d29c189b8fb58
Hugn71205 <diablo.hugn@gmail.com>
2025-05-17 14:18:21 +0700

    fix:fix lai duong dan base

--- src/routes/main.route.tsx
+++ src/routes/main.route.tsx
@@ -1,11 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "../components/layouts/MainLayout"
 import HomePage from "../pages/HomePage"
-import ProductListingPage from "../pages/ProductListingPage"
-import ProductDetailPage from "../pages/ProductDetailPage"
-import CartPage from "../pages/CartPage"
-import CheckoutPage from "../pages/CheckoutPage"
-import AccountPage from "../pages/AccountPage"
 import LoginPage from "../pages/LoginPage"
 import SignUpPage from "../pages/SignUpPage"
 
@@ -15,46 +10,6 @@ export const router = createBrowserRouter([
     element: (
       <MainLayout>
         <HomePage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/products",
-    element: (
-      <MainLayout>
-        <ProductListingPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/product/:id",
-    element: (
-      <MainLayout>
-        <ProductDetailPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/cart",
-    element: (
-      <MainLayout>
-        <CartPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/checkout",
-    element: (
-      <MainLayout>
-        <CheckoutPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/account",
-    element: (
-      <MainLayout>
-        <AccountPage />
       </MainLayout>
     ),
   },
