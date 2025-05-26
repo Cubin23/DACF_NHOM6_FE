526ac424e8804bda3c318f5506dbb4c3acf944a8
cubin85 <hungabc8511@gmail.com>
2025-05-27 02:27:39 +0700

    resetpassword

--- src/routes/main.route.tsx
+++ src/routes/main.route.tsx
@@ -10,11 +10,14 @@ import AccountPage from "../pages/AccountPage"
 import SignUpPage from "../pages/SignUp/SignUpPage"
 import LoginPage from "../pages/LoginPage"
 import VerifyEmail from "../pages/VerifyEmail"
+import Forgotpassword from "../pages/Forgot-password"
+import VerifyOtp from "../pages/VerifyOtp"       // bạn tạo file này
+import ResetPassword from "../pages/ResetPassword" // bạn tạo file này
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />, // KHÔNG cần truyền children
+    element: <MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "products", element: <ProductListingPage /> },
@@ -25,9 +28,9 @@ export const router = createBrowserRouter([
       { path: "login", element: <LoginPage /> },
       { path: "register", element: <SignUpPage /> },
       { path: "verify-email", element: <VerifyEmail /> },
-
-      
+      { path: "forgot-password", element: <Forgotpassword /> },
+      { path: "forgot-password/verify-otp", element: <VerifyOtp /> },
+      { path: "forgot-password/reset", element: <ResetPassword /> },
     ],
   },
-
 ])
